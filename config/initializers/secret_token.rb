ed78265ce13339919a20e731e14063b60fd92d4b
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-01-03 02:24:09 +0100

    add config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRestApiBootstrap::Application.config.secret_token = '0b39734ab6f00a3fedec8086690548a4e0dff1120a26c52aa05b903c53d0cd34c4edcb39eaf150d17d6295c8850018f982aefccc734e0fdb611855e9a0207451'
