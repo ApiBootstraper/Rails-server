98d7a8b93b14aaf9ac16fa92071f073ac53d8e56
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-01-05 19:46:03 +0100

    Clean migrations

--- db/migrate/20120325191502_update_user.rb
+++ db/migrate/20120325191502_update_user.rb
@@ -5,9 +5,11 @@ class UpdateUser < ActiveRecord::Migration
     add_column :users, :is_enable, :boolean, :default => true
     add_column :users, :enabled_at, :datetime
     add_index  :users, :is_enable
+    add_index  :users, :uuid
   end
 
  def down
+    remove_index  :users, :uuid
     remove_index  :users, :is_enable
     remove_column :users, :enabled_at
     remove_column :users, :is_enable
--- db/migrate/20120330202235_create_trackings.rb
+++ db/migrate/20120330202235_create_trackings.rb
@@ -1,14 +1,15 @@
 class CreateTrackings < ActiveRecord::Migration
   def change
     create_table :trackings do |t|
-      t.string :uuid
-      t.string :request
-      t.string :method
+      t.string  :uuid
+      t.string  :request
+      t.string  :method
       t.integer :code
-      t.string :remote_ip
-      t.string :version
+      t.string  :remote_ip
+      t.string  :version
 
       t.timestamps
     end
+    add_index :trackings, :uuid
   end
 end
