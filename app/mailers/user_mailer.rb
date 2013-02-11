13d6eb6ace611d7451406ce092b0d940ed9505e4
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-02-11 23:56:04 +0100

    Update mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < BaseMailer
 
   def welcome_email(user)
     @user = user
-    @url  = "http://example.com/login"
+    @url  = "http://apibootstraper.com/login"
     mail(:to => user.email, :subject => "Welcome to example.com")
   end
 
