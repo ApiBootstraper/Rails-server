4a5e93d6b83c0f68f333ada0ecbaafcac47fd9f9
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-03-05 23:40:45 +0100

    Update db/seeds.rb
    example.com → apibootstraper.com

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # Create a default user
-AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
-User.create!(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password', :username => 'demo')
+AdminUser.create!(:email => 'admin@apibootstraper.com', :password => 'password', :password_confirmation => 'password')
+User.create!(:email => 'user@apibootstraper.com', :password => 'password', :password_confirmation => 'password', :username => 'demo')
 
 Application.create!(:name => 'Demo Application') # Add this in Rails4 -- :is_enable => true)
